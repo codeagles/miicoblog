868c7ddcd334a0c1a4d3d09f2ae7a4ac295f2aaa
codeagles <hcn0521@outlook.com>
2024-06-10 17:29:00 +0800

    nav

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,14 @@ export default defineUserConfig({
         // more...
         search: {
             // more options
-        }
+        },
+        navbar: [
+            { text: '首页', link: '/', icon: 'material-symbols:home-outline' },
+            { text: '博客', link: '/blog/', icon: 'material-symbols:article-outline' },
+            { text: 'Github', link: 'https://github.com/codeagles', icon: 'material-symbols:notifications' },
+            { text: '关于我', link: '/about', icon: 'material-symbols:face' },
+        ]
     }),
     bundler: viteBundler(),
+
 })
