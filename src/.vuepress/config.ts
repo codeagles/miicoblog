7e13fe778fe79a943303aa0b7f27dc9e9806b10a
houchunan <hcn0521@outlook.com>
2025-03-19 14:31:38 +0800

    adsense

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -9,8 +9,16 @@ export default defineUserConfig({
   description: "Miico的博客-码海拾贝",
 
   theme,
-
-
+  head: [
+    [
+      "script",
+      {
+        "data-ad-client": "ca-pub-3132583005274003",
+        async: true,
+        src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3132583005274003"
+      }
+    ]
+  ]
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
